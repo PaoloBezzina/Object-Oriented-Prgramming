86ae23a0d3e977086e66feaac57cc78d3f3dbeea
Paolo Bezzina <paolo.bezzina.19@um.edu.mt>
2021-02-05 14:49:16 +0100

    ok

---
+++ Task2 V2/Person.java
@@ -0,0 +1,87 @@
+public class Person {
+    
+    //int no;
+    String id;
+    String name;
+    String surname;
+    String title;
+    String role;
+    String gender;
+    ArrayList<String> actions = new ArrayList<String>();
+
+    //constructor
+    public Person(String id, String name, String surname, String title, String role, String gender){
+        setId(id);
+        setName(name);
+        setSurname(surname);
+        setGender(gender);
+        setRole(role);
+        setTitle(title);
+    }
+    
+    //Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //Getters
+    public String getID() {
+        return ID;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getRole() {
+        return role;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getFullName() {
+        return name + surname;
+    }
+
+    public ArrayList<String> getActions() {
+        return actions;
+    }
+
+
+    
+    public void initActions(){
+
+    }
+
+    public void addAction(){
+
+    }
+
+    public void displayActions(){
+
+    }
+
+    public void personDetails(){
+
+    }
+    
+}
--- Task2/Card.java
+++ Task2/Card.java
@@ -1,32 +1,32 @@
-import java.security.acl.Owner;
+import java.util.Date;
+import java.util.Calendar;  
+import java.text.DateFormat;  
+import java.text.SimpleDateFormat;  
 
 public class Card {
     
     String cardNo;
+    Account account;
+    String owner;
     int cvv;
     String expiryDate;
-    AccountHolder owner;
-    Account acc;
 
-    public void setAcc(Account acc) {
-        this.acc = acc;
-    }
-    public void setCardNo(String cardNo) {
-        this.cardNo = cardNo;
-    }
-    public void setCvv(int cvv) {
-        this.cvv = cvv;
-    }
-    public void setExpiryDate(String expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-    public void setOwner(AccountHolder owner) {
-        this.owner = owner;
+    public Card(Account acc, String name){
+        numberOfCards++;
+        DateFormat dateFormat = new SimpleDateFormat("yy-mm");
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.YEAR, 4);
+        Date date = c.getTime();
+        expiryDate = dateFormat.format(date);
+        cvv = (Math.random() * (999) +1);
+        cardNo = (Math.random() * (99999999) +1);
+        cardNo += String.format("%04d", numberOfCards);
+        account = acc;
+        owner = name;
     }
 
-
-    public Account getAcc() {
-        return acc;
+    public Account getAccount() {
+        return account;
     }
     public String getCardNo() {
         return cardNo;
@@ -41,7 +41,6 @@ public class Card {
         return owner;
     }
 
-    //...CVV(): void
     //'check' ExpDate: String
     //displayCard(): void
 }
