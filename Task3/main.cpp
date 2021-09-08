997d28fbbb417176bb4c6e2bba5c2b68b25d4cf7
paolo.bezzina <paolo.bezzina.19@um.edu.mt>
2021-09-08 14:17:33 +0200

    added main class to run library test

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "myuint.hpp"
+
 void test();
 
 int main()
@@ -11,6 +12,20 @@ int main()
 
 void test()
 {
-    myuint<1024> i(5); // creates a 1024-bit unsigned int '5'
-    std::cout << getNumberAsString(i);
+    myuint<1024> a = (40000);
+    std::cout << a.getNumber() << std::endl;
+    std::cout << a.convert_to<int>() << std::endl;
+
+    myuint<1024> b(3);
+    std::cout << b.getNumber() << std::endl;
+    std::cout << b.convert_to<int>() << std::endl;
+
+    std::cout << ((a * b) / 2).convert_to<int>() << std::endl;
+}
+
+int test2()
+{
+    myuint<1024> i(5);                  // creates a 1024-bit unsigned int '5'
+    myuint<2048> j = i << 1000 + 23;    // shifts it by 1000 bits and adds 23
+    return j.template convert_to<int>() // returns 23
 }
